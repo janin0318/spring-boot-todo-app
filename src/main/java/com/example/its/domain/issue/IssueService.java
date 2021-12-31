41643492c593782b0579206108740be624b24ba6
janin0318 <janin-ou@outlook.jp>
2022-01-01 00:02:28 +0900

    first commit

---
+++ src/main/java/com/example/its/domain/issue/IssueService.java
@@ -0,0 +1,17 @@
+package com.example.its.domain.issue;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class IssueService {
+
+  private final IssueRepository issueRepository;
+
+  public List<IssueEntity> findAll() {
+    return issueRepository.findAll();
+  }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into issues (summary, description) values ('バグA', 'バグAがあります');
+insert into issues (summary, description) values ('機能要望B', '機能要望Bがほしいです');
+insert into issues (summary, description) values ('画面Cが遅い', '早くしてほしいです');
